80c15a4705fec764af233f3b1e5a9ae251e114e5
hammadmajid <hammadmajid@proton.me>
2024-10-11 21:01:47 +0500

    add primary and secondary font

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { lusitana } from "@/app/ui/fonts";
 
 export default function Page() {
   return (
@@ -12,6 +13,9 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          <p
+            className={`${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
